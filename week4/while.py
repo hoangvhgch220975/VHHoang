3ddd4afad21c56e9fc2ef51339196fbe1d740738
hoangvhgch220975 <ptahoangvh2k4@gmail.com>
2023-07-14 10:07:37 +0700

    git push all

--- week4/while.py
+++ week4/while.py
@@ -1,12 +1,12 @@
-#while scenario 1: asking user to continue
-# stop = False
-# while not stop: 
-#     product = input('Enter product: ')
-#     sold = int(input('Enter sold: '))
-#     price = float(input('Enter price: '))
-#     print('Product: ', product, 'Sold: ', sold, 'Price: ', price)
-#     anwser =input('Continue? (y/n): ')
-#     stop = anwser == 'n'
+# while scenario 1: asking user to continue
+stop = False
+while not stop: 
+    product = input('Enter product: ')
+    sold = int(input('Enter sold: '))
+    price = float(input('Enter price: '))
+    print('Product: ', product, 'Sold: ', sold, 'Price: ', price)
+    anwser =input('Continue? (y/n): ')
+    stop = anwser == 'n'
 
 #while scenario 2: validating values
 valid_input = False
